8e16f661db56bfb4ab79b310f5aa5dd9b1587dca
Gengelenok <144037423+Gengelenok@users.noreply.github.com>
2024-11-26 16:45:37 +0400

    update README.md update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ try:
 
     isPyperclip = True
 except:
-    print("Please install pyperclip, for better experience")
+    print("Install pyperclip for better experience")
     pass
 
-commandBlock_commandWorkpiece = """summon minecraft:falling_block ~ ~1 ~ {BlockState:{Name:"redstone_block"},Time:1,Passengers:[{id:armor_stand,Health:0,Passengers:[{id:falling_block,BlockState:{Name:"activator_rail"},Time:1,Passengers:[replace77k46gob8cCH%eQNwcc8Zk9Vxw,{id:command_block_minecart,Command:'setblock ~ ~1 ~ command_block{auto:1,Command:"kill @e[type=command_block_minecart,distance=..3]"}'},{id:command_block_minecart,Command:'setblock ~ ~2 ~ command_block{auto:1,Command:"fill ~ ~ ~ ~ ~-3 ~ air"}'}]}]}]}"""
-command_workpiece = "{id:command_block_minecart,Command:'replace77k46gob8cCH%eQNwcc8Zk9Vxw'}"
-rep = 'replace77k46gob8cCH%eQNwcc8Zk9Vxw'
+commandBlock_commandWorkpiece = """summon minecraft:falling_block ~ ~1 ~ {BlockState:{Name:"redstone_block"},Time:1,Passengers:[{id:armor_stand,Health:0,Passengers:[{id:falling_block,BlockState:{Name:"activator_rail"},Time:1,Passengers:[replace77k46gob8cCH%eQNwcc8Zk9VxwReplace,{id:command_block_minecart,Command:'setblock ~ ~1 ~ command_block{auto:1,Command:"kill @e[type=command_block_minecart,distance=..3]"}'},{id:command_block_minecart,Command:'setblock ~ ~2 ~ command_block{auto:1,Command:"fill ~ ~ ~ ~ ~-3 ~ air"}'}]}]}]}"""
+command_workpiece = "{id:command_block_minecart,Command:'replace77k46gob8cCH%eQNwcc8Zk9VxwReplace'}"
+rep = 'replace77k46gob8cCH%eQNwcc8Zk9VxwReplace'
 commands = []
 
 
@@ -23,13 +23,16 @@ def createCommand(commandBlock_commandWorkpiece, command_workpiece, commands, re
 
 
 while 1:
-    tmp = input('Enter command (\'\' for end): ')
+    tmp = input('Enter command (empty for end): ')
     if tmp != '':
         commands.append(tmp.replace('/', ''))
     else:
         command = createCommand(commandBlock_commandWorkpiece, command_workpiece, commands)
         print(command)
         if isPyperclip:
-            pyperclip.copy(command)
+            try:
+                pyperclip.copy(command)
+            except:
+                pass
         input()
         break
